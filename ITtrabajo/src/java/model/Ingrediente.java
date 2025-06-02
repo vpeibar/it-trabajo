a7f7e7d3a6f590037682cb5196c03b4837391c50
vpeibar <vpeibar@alu.upo.es>
2025-06-02 18:48:59 +0200

    he hecho parte del modelo

--- ITtrabajo/src/java/model/Ingrediente.java
+++ ITtrabajo/src/java/model/Ingrediente.java
@@ -10,5 +10,29 @@ package model;
  * @author maria
  */
 public class Ingrediente {
+    private int id;
+    private String nombre;
+
+    public Ingrediente(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
+  
 }
