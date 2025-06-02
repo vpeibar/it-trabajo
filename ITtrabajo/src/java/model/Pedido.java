a1d54b838c30466bdd18c90010b22a37ad28e711
mxriodia <mxriodia@gmail.com>
2025-06-02 18:50:16 +0200

    he añadido las clases categoria, linea pedido y pedido al modelo

--- ITtrabajo/src/java/model/Pedido.java
+++ ITtrabajo/src/java/model/Pedido.java
@@ -5,10 +5,54 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author maria
  */
 public class Pedido {
-    
+    private int id;
+    private int cliente_id;
+    private Date fecha;
+    private String estado;
+
+    public Pedido(int id, int cliente_id, Date fecha, String estado) {
+        this.id = id;
+        this.cliente_id = cliente_id;
+        this.fecha = fecha;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(int cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
