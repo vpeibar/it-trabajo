d99c38b151f3e1914351b3b9a1a0e47fb00409bf
vpeibar <vpeibar@alu.upo.es>
2025-06-03 13:21:33 +0200

    RESTFUL DEL PLATO

--- webservice/src/java/restful/PlatoResource.java
+++ webservice/src/java/restful/PlatoResource.java
@@ -5,14 +5,20 @@
  */
 package restful;
 
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import model.Plato;
 
 /**
  * REST Web Service
@@ -33,21 +39,50 @@ public class PlatoResource {
 
     /**
      * Retrieves representation of an instance of restful.PlatoResource
+     *
      * @return an instance of java.lang.String
      */
     @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public String getXml() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public List<Plato> listar() {
+        return DAOPlato.listarTodos();
     }
 
-    /**
-     * PUT method for updating or creating an instance of PlatoResource
-     * @param content representation for the resource
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_XML)
-    public void putXml(String content) {
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Plato obtener(@PathParam("id") int id) {
+        return DAOPlato.obtenerPorId(id);
     }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response crear(Plato plato) {
+        DAOPlato.crear(plato);
+        return Response.status(Response.Status.CREATED).entity(plato).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response actualizar(@PathParam("id") int id, Plato plato) {
+        plato.setId(id);
+        return DAOPlato.actualizar(plato)
+                ? Response.ok(plato).build()
+                : Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response eliminar(@PathParam("id") int id) {
+        return DAOPlato.eliminar(id)
+                ? Response.ok().build()
+                : Response.status(Response.Status.NOT_FOUND).build();
+    }
+
 }
